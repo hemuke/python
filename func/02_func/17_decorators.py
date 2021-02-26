72f07ba5db515718a80d1008efaff09a2caa8442
hemuke <hemuke@126.com>
2021-02-26 18:18:36 +0800

    修改装饰器

--- func/02_func/17_decorators.py
+++ func/02_func/17_decorators.py
@@ -6,6 +6,11 @@ def hi():
  
 def doSomethingBeforeHi(func):
     print("I am doing some boring work before executing hi()")
-    print(func)
+    print(func)				#<function hi at 0x7f0ea36a61f0>
+    print("func", func.__name__)	#hi
  
 doSomethingBeforeHi(hi)
+
+print()
+print(hi.__name__)			#hi
+print(doSomethingBeforeHi.__name__)	#doSomethingBeforeHi	
