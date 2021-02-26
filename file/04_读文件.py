244fc6bdf9eb77a3a5156e262d7bfaa1f30804eb
hemuke <hemuke@126.com>
2021-02-26 13:52:57 +0800

    更新文件

---
+++ file/04_读文件.py
@@ -0,0 +1,14 @@
+#! /root/anaconda3/bin/python
+"""
+(base) [root@k8s-ansible file]# cat myfile.txt 
+1234567890
+abcdefghijklmn
+opqrstuvwxyz
+"""
+# readline参数
+file = open('myfile.txt')
+print(file.readline())
+print(file.readline(11))
+print(file.readline())
+print(file.readline(-5))
+file.close()
