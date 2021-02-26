3eccba1452d5e2f50c7e67c946d611ecbaefdc86
hemuke <hemuke@126.com>
2021-02-26 18:12:13 +0800

    装饰器修改

--- func/02_func/19_decorators.py
+++ func/02_func/19_decorators.py
@@ -12,15 +12,10 @@ def a_new_decorator(a_func):
  
     return wrapTheFunction
  
+@a_new_decorator # a_function_requiring_decoration = a_new_decorator(a_function_requiring_decoration)
 def a_function_requiring_decoration():
-    print("I am the function which needs some decoration to remove my foul smell")
- 
-#a_function_requiring_decoration()
-# 
-#a_new_decorator(a_function_requiring_decoration)()
-#
-a_function_requiring_decoration = a_new_decorator(a_function_requiring_decoration)
-
-print(a_function_requiring_decoration.__name__)
+    print("I am the function which needs some decoration to "
+          "remove my foul smell")
  
 a_function_requiring_decoration()
+print(a_function_requiring_decoration.__name__)
