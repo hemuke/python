3f99c7983a683b1eb31b3658f4ad8b7221dd9b08
hemuke <hemuke@126.com>
2021-03-01 18:17:15 +0800

    exception

---
+++ exception/06_customize_except.py
@@ -0,0 +1,19 @@
+#! /root/anaconda3/bin/python
+
+class MyException(Exception):
+    def __init__(self, msg1, msg2):
+        self.msg1 = msg1
+        self.msg2 = msg2
+
+try:
+    raise MyException("123", "abc")
+except Exception:
+    print(Exception)
+
+print("ABC")
+
+
+try:
+    raise MyException("123", "abc")
+except MyException as err:
+    print(err)
