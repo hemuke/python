cbd48ae12d55e39fee16a95c139bb5a4242a6ea6
hemuke <hemuke@126.com>
2021-03-01 09:50:00 +0800

    modify 18_decorators.py

--- func/02_func/18_decorators.py
+++ func/02_func/18_decorators.py
@@ -15,10 +15,12 @@ def a_new_decorator(a_func):
 def a_function_requiring_decoration():
     print("I am the function which needs some decoration to remove my foul smell")
  
-#a_function_requiring_decoration()
-# 
-#a_new_decorator(a_function_requiring_decoration)()
-#
+a_function_requiring_decoration()
+ 
+a_new_decorator(a_function_requiring_decoration)()
+
+print(a_new_decorator(a_function_requiring_decoration).__name__)
+
 a_function_requiring_decoration = a_new_decorator(a_function_requiring_decoration)
 
 print(a_function_requiring_decoration.__name__)
