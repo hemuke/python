4ab2416b24bbd44f8f4b33766ddd8546d63239dc
hemuke <hemuke@126.com>
2021-03-02 09:43:12 +0800

    03_try-except-else.py

--- exception/03_try-except-else.py
+++ exception/03_try-except-else.py
@@ -14,11 +14,20 @@ print("结束")
 
 ############################################
 
+#while True:
+#    try:
+#        x = int(input("请输入一个整数："))
+#    except ValueError:
+#        print("无效的输入，请再次输入")
+#    else:
+#        print("输入的整数为：", x)
+#        break
+
 while True:
     try:
-        x = int(input("请输入一个整数："))
-    except ValueError:
-        print("无效的输入，请再次输入")
+        x = int(input("请输入一个整数:"))
+    except Exception:
+        print(Exception)
     else:
         print("输入的整数为：", x)
         break
