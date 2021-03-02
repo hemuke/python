ca0b8c4015de90b8029d95f76172f59512eb240e
hemuke <hemuke@126.com>
2021-03-02 14:10:58 +0800

    data_type

---
+++ data_type/01_bool.py
@@ -0,0 +1,12 @@
+#! /root/anaconda3/bin/python
+import sys
+
+print(sys.getsizeof(True))	#28 
+print(sys.getsizeof(False))	#24
+
+print(5 > 3)
+print(5 < 3)
+
+print(True == 1)
+print(False == 0)
+print(True + False + 5)
