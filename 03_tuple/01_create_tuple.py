0e0951282d58ef8c4ef3ac8c011b01a0487d2d33
hemuke <hemuke@126.com>
2021-03-03 16:43:07 +0800

    tuple类型

---
+++ 03_tuple/01_create_tuple.py
@@ -0,0 +1,13 @@
+#! /root/anaconda3/bin/python
+t = ('Python', 18, True)
+print(t)
+
+t = 'Python', 18, True
+print(t)
+
+print(())
+print(tuple())
+
+t = (5, [1, 3], 8)
+t[1][1] = 7
+print(t)
---
+++ 03_tuple/02_only_one_tuple.py
@@ -0,0 +1,8 @@
+#! /root/anaconda3/bin/python
+t = (18)
+print(t)
+print(type(t))
+
+t = (18,)
+print(t)
+print(type(t))
