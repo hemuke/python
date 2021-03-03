17c7092fb0f5c441e1338d170c28b8108ef4a2b6
hemuke <hemuke@126.com>
2021-03-03 10:57:46 +0800

    02_lambda.py

--- func/02_func/02_lambda.py
+++ func/02_func/02_lambda.py
@@ -1,4 +1,6 @@
 #! /root/anaconda3/bin/python
+from collections.abc import Iterator
+from collections.abc import Iterable
 # 例子一
 def add(num1, num2):
     return num1 + num2
@@ -13,7 +15,15 @@ print(le(5, 6))
 
 # 例子三
 result = map(lambda x: x * x, [1, 2, 3])
+print(next(result))
+print(next(result))
+print(next(result))
 print(result)
+print(isinstance(result, Iterator))
+print(isinstance(result, Iterable))
+
+
+result = map(lambda x: x * x, [1, 2, 3])
 print(list(result))
 
 # 例子四
