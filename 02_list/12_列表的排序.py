ee9d298b425298c338009b99b6b78cd8134bfa79
hemuke <hemuke@126.com>
2021-03-03 16:28:49 +0800

    列表

---
+++ 02_list/12_列表的排序.py
@@ -0,0 +1,18 @@
+#! /root/anaconda3/bin/python
+L = [5, 3, 8, 1, 6]
+
+L.sort()
+print(L)
+
+L.sort(reverse=True)
+print(L)
+
+###########################################
+
+L = [5, 3, 8, 1, 6]
+
+print(sorted(L))
+print(L)
+
+print(sorted(L, reverse = True))
+print(L)
