8a33042846cb6c7ad4852c9761f33eca383e6e08
hemuke <hemuke@126.com>
2021-03-04 14:57:38 +0800

    change_dict

--- 04_dict/03_change_dict.py
+++ 04_dict/03_change_dict.py
@@ -4,5 +4,7 @@ d['age'] = 20
 print(d)
 
 d = {'name': 'Jack', 'age': 18, 'gender': '男'}
-d.update(name='Mike', age=20)
+#d.update(name='Mike', age=20)
+#d.update([('name', 'Mike'), ('age', 20)])
+d.update({'name': 'Mike', 'age': 20})
 print(d)
