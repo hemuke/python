ba24f0632a8016cfc5c62b390468c212a7243694
hemuke <hemuke@126.com>
2021-03-05 16:07:08 +0800

    06_lambda_exercice

--- 09_func/03_func/06_lambda_exercise.py
+++ 09_func/03_func/06_lambda_exercise.py
@@ -7,9 +7,9 @@ zodiac_days = ((1, 20), (2, 19), (3, 21), (4, 21),
                (5, 21), (6, 22), (7, 23), (8, 23),
                (9, 23), (10, 23), (11, 23), (12, 23))
 
-(month, day) = (2, 15)
+(month, day) = (2, 22)
 
-zodiac_day = filter(lambda x: x<=(month, day), zodiac_days)
+zodiac_day = filter(lambda x: x<(month, day), zodiac_days)
 # print(list(zodiac_day))
 
 zodiac_len = len(list(zodiac_day)) % 12
