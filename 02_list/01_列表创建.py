365ad2e020109000e3c788059f51c053c3d85174
hemuke <hemuke@126.com>
2021-03-05 09:52:11 +0800

    01_列表创建

--- 02_list/01_列表创建.py
+++ 02_list/01_列表创建.py
@@ -8,5 +8,6 @@ print(list(range(1, 6)))
 print(list(['Python', 18, True]))
 print(list(('Python', 18, True)))
 print(list({'Python', 18, True}))
+print(list({'sub': 'Python', 'i': 18, 't': True}))
 
 print(list())
