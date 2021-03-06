bd6e145c44a910e4fcfafc092ed5dd45e3af4f19
hemuke <hemuke@126.com>
2021-03-06 14:23:46 +0800

    new decorators

---
+++ 09_func/03_func/09_closure_exercise.py
@@ -0,0 +1,13 @@
+#! /root/anaconda3/bin/python
+def a_line(a, b):
+
+    def arg_y(y):
+        return a * x + b
+
+    return arg_y
+
+
+line1 = a_line(3, 5)
+line2 = a_line(5, 10)
+print(line1(10))
+print(line1(20))
