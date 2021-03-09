3fe6ce421ddff89a9ea72a37c0065b12a5c2fb5a
hemuke <hemuke@126.com>
2021-03-09 11:48:05 +0800

    08_group_match.py

--- 16_re/01_re/08_group_match.py
+++ 16_re/01_re/08_group_match.py
@@ -13,10 +13,9 @@ matchobj = re.match(r'(?P<fir>\d+)-(?P<sec>\d+)', '025-3456abc')
 matchobj = re.compile(r'(?P<fir>\d+)-(?P<sec>\d+)').match('025-3456abc')
 print(matchobj.group('fir'))
 print(matchobj.group('sec'))
-# 报错
-# print(matchobj.group(1))
-# print(matchobj.group(2))
-# print(matchobj.group(0))
+print(matchobj.group(1))
+print(matchobj.group(2))
+print(matchobj.group(0))
 
 print(re.match(r'(\d+)-(\d+)-(\2)', '025-3456-3456789'))
 print(re.compile(r'(\d+)-(\d+)-(\2)').match('025-3456-3456789'))
