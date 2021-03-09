ef2cb75fc173ffa78c683c834d7a1fabf9656c6a
hemuke <hemuke@126.com>
2021-03-09 10:01:20 +0800

    02_re

---
+++ 16_re/02_re/09_match_attribute_func.py
@@ -0,0 +1,28 @@
+#! /root/anaconda3/bin/python
+import re
+
+match_obj = re.compile(r'(\d+)-(?P<sec>\d+)').match('025-3456abc', 1, 7)
+print(match_obj)
+
+print(match_obj.string)
+print(match_obj.re)
+print(match_obj.pos)
+print(match_obj.endpos)
+print(match_obj.lastindex)
+print(match_obj.lastgroup)
+
+print(match_obj.group(2))
+print(match_obj.group('sec'))
+print(match_obj.group())
+print(match_obj.group(0))
+print(match_obj.group(1, 'sec'))
+
+print(match_obj.groups())
+print(match_obj.groupdict())
+
+print(match_obj.start('sec'))
+print(match_obj.start())
+print(match_obj.end('sec'))
+print(match_obj.end())
+
+print(match_obj.span('sec'))
