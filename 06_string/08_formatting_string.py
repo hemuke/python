bfb6aa252782e2665aba8d8b966df5b58ac2d650
hemuke <hemuke@126.com>
2021-03-11 10:40:22 +0800

    08_formatting_string.py

--- 06_string/08_formatting_string.py
+++ 06_string/08_formatting_string.py
@@ -10,9 +10,11 @@ print(f'父进程启动{pid}-{name}')
 
 
 def do_sth(arg1, arg2):
-     print(f'子进程启动{pid}-{name}')
+     child_pid = current_process().pid
+     child_name = current_process().name
+     print(f'子进程启动{child_pid}-{child_name}')
      print(f'arg1 = {arg1}, arg2 = {arg2}')
-     print(f'子进程结束{pid}-{name}')
+     print(f'子进程结束{child_pid}-{child_name}')
 
 
 process = Process(target=do_sth, args=(5, 8))
